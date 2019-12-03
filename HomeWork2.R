e3b600bdf0118057997786e22f4b4ef6938b08a6
Angel Igareta <angel@igareta.com>
2019-12-03 19:48:39 +0100

    Added first model approach

--- HomeWork2.R
+++ HomeWork2.R
@@ -1,1 +1,16 @@
-data <- read.table("./data.txt",quote="\"", comment.char="", stringsAsFactors = F)
+library(ggplot2)
+library(dplyr)
+
+data <- read.table("./data.txt",quote="\"", comment.char="", stringsAsFactors = T)
+
+# Rename headers from data
+names(data) <- c("Caratage", "ColourPurity", "Clarity", "InstCert", "Price_SGD")
+
+# Plot price vs caratage
+data %>% ggplot(aes(x = Caratage, y = Price_SGD)) + geom_point()
+
+# Plot log price vs caratage (As it is a transformation, we avoid heterocedasticity)
+data %>% ggplot(aes(x = Caratage, y = log(Price_SGD))) + geom_point()
+
+# We are going to use log price since the explanatory variable is better
+data %>% mutate("Log_Price_SGD" = log(Price_SGD))
